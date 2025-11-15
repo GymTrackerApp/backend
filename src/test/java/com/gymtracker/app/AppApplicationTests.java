e6aae0881fd520332e25c82d7104c51b42dd85ce
Szymon <szymongryz@interia.pl>
2025-11-15 17:46:28 +0100

    add testcontainers

--- src/test/java/com/gymtracker/app/AppApplicationTests.java
+++ src/test/java/com/gymtracker/app/AppApplicationTests.java
@@ -2,12 +2,28 @@ package com.gymtracker.app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 class AppApplicationTests {
+
+    @Container
+    static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:18.1-alpine");
+
+    @DynamicPropertySource
+    static void setApplicationProperties(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
+
+        registry.add("spring.datasource.username", postgreSQLContainer::getUsername);
+        registry.add("spring.datasource.password", postgreSQLContainer::getPassword);
+    }
 
 	@Test
 	void contextLoads() {
 	}
-
 }
