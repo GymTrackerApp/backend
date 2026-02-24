19bc95408c0c85047f896d7a22545e405e446531
Szymon <szymongryz@interia.pl>
2026-02-24 17:48:27 +0100

    add additional field - id to the plan_items table

--- src/main/resources/db/migration/V12__add_position_field_to_plan_items_table.sql
+++ src/main/resources/db/migration/V12__add_position_field_to_plan_items_table.sql
@@ -1,10 +1,13 @@
 ALTER TABLE plan_items 
 ADD COLUMN position INTEGER;
 
+ALTER TABLE plan_items
+ADD COLUMN id BIGSERIAL UNIQUE;
+
 UPDATE plan_items
 SET position = subquery.new_pos
-FROM (SELECT exercise_id, training_plan_id, row_number() over (partition by training_plan_id) - 1 as new_pos FROM plan_items) AS subquery
-WHERE plan_items.exercise_id = subquery.exercise_id and plan_items.training_plan_id = subquery.training_plan_id;
+FROM (SELECT id, row_number() over (partition by training_plan_id order by id) - 1 as new_pos FROM plan_items) AS subquery
+WHERE plan_items.id = subquery.id;
 
 ALTER TABLE plan_items
 ALTER COLUMN position SET NOT NULL
